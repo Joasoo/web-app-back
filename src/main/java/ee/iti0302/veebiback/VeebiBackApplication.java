7bf571e6e323cbd6a208122b041e95aeca9ba1c5
alllip <alllip@ttu.ee>
2023-09-06 20:23:47 +0300

    Initial setup.

---
+++ src/main/java/ee/iti0302/veebiback/VeebiBackApplication.java
@@ -0,0 +1,13 @@
+package ee.iti0302.veebiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeebiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeebiBackApplication.class, args);
+    }
+
+}
