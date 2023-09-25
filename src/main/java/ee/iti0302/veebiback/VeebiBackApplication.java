52062c4acd8482cd33cc7f3af9d936d7b30f5845
alllip <alllip@ttu.ee>
2023-09-25 18:19:24 +0300

    Spring Security default user/password.

--- src/main/java/ee/iti0302/veebiback/VeebiBackApplication.java
+++ src/main/java/ee/iti0302/veebiback/VeebiBackApplication.java
@@ -3,10 +3,9 @@ package ee.iti0302.veebiback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 // Temporarily disable spring Security and datasource.
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class VeebiBackApplication {
 
     public static void main(String[] args) {
