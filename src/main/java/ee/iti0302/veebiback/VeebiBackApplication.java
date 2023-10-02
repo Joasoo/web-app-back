b6ec5a354c24570d4d57673ee67578aa8633c0a1
alllip <alllip@ttu.ee>
2023-10-02 23:01:09 +0300

    Liquibase table config.

--- src/main/java/ee/iti0302/veebiback/VeebiBackApplication.java
+++ src/main/java/ee/iti0302/veebiback/VeebiBackApplication.java
@@ -2,10 +2,8 @@ package ee.iti0302.veebiback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// Temporarily disable spring Security and datasource.
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class VeebiBackApplication {
 
     public static void main(String[] args) {
