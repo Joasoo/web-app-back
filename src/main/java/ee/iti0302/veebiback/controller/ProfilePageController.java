c5d488d1906b6e3e4d811565775ed5e9329896a4
Markus <majoas@ttu.ee>
2023-10-07 01:19:58 +0300

    enuf for today

--- src/main/java/ee/iti0302/veebiback/controller/ProfilePageController.java
+++ src/main/java/ee/iti0302/veebiback/controller/ProfilePageController.java
@@ -1,6 +1,8 @@
 package ee.iti0302.veebiback.controller;
 
 import ee.iti0302.veebiback.dto.ProfileDataDto;
+import ee.iti0302.veebiback.service.ProfilePageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/profile")
+@RequiredArgsConstructor
 public class ProfilePageController {
-
+    private final ProfilePageService service;
     @GetMapping
     public ProfileDataDto getProfileData(@RequestParam int id) {
         ProfileDataDto testDto = new ProfileDataDto();
