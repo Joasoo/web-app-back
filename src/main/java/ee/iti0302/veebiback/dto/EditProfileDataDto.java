39ba6608b3c5602a5af4199648b21d064b85ef02
Johannes <jojurg@ttu.ee>
2023-10-08 22:01:56 +0300

    started edit-page backend

--- src/main/java/ee/iti0302/veebiback/dto/EditProfileDataDto.java
+++ src/main/java/ee/iti0302/veebiback/dto/EditProfileDataDto.java
@@ -5,9 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class EditProfileDataDto {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String email;
+    //private String relationshipStatus;
+    private String residence;
+    private String hometown;
+    private String workplace;
+    private String bio;
 }
