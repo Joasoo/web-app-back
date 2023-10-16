9ac8194881d6551f479f2fbe95e8e39a24b7c641
Markus <majoas@ttu.ee>
2023-10-16 18:35:36 +0300

    Remove empy test so we can build jar

--- src/test/java/ee/iti0302/veebiback/VeebiBackApplicationTests.java
+++ src/test/java/ee/iti0302/veebiback/VeebiBackApplicationTests.java
@@ -5,9 +5,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VeebiBackApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+    // TODO: Add tests
 }
