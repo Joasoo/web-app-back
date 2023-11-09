4933202f22cceb6f1b9ebcb4921957cd03f2bcf6
Markus <majoas@ttu.ee>
2023-11-09 17:14:50 +0200

    Friend request refactor using statuscodes.

--- src/main/java/ee/iti0302/veebiback/service/mapper/FriendshipMapper.java
+++ src/main/java/ee/iti0302/veebiback/service/mapper/FriendshipMapper.java
@@ -1,18 +1,15 @@
 package ee.iti0302.veebiback.service.mapper;
 
 import ee.iti0302.veebiback.domain.Friendship;
-import ee.iti0302.veebiback.dto.FriendshipDto;
+import ee.iti0302.veebiback.dto.FriendListDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FriendshipMapper {
-    @Mapping(source = "person.id", target = "person.id")
-    @Mapping(source = "person.firstName", target = "person.firstName")
-    @Mapping(source = "person.lastName", target = "person.lastName")
-    @Mapping(source = "friend.id", target = "friend.id")
-    @Mapping(source = "friend.firstName", target = "friend.firstName")
-    @Mapping(source = "friend.lastName", target = "friend.lastName")
-    FriendshipDto toDto(Friendship friendship);
+    @Mapping(source = "friend.id", target = "id")
+    @Mapping(source = "friend.firstName", target = "name.firstName")
+    @Mapping(source = "friend.lastName", target = "name.lastName")
+    FriendListDto toFriendListDto(Friendship friendship);
 }
