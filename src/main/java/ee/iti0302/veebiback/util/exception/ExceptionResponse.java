b6339db49b41c87bbf8793d38ae6de2f547cad63
alllip <alllip@ttu.ee>
2023-11-15 20:38:17 +0200

    error handling

--- src/main/java/ee/iti0302/veebiback/util/exception/ExceptionResponse.java
+++ src/main/java/ee/iti0302/veebiback/util/exception/ExceptionResponse.java
@@ -3,14 +3,19 @@ package ee.iti0302.veebiback.util.exception;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class ExceptionResponse {
     private String path;
+    private LocalDateTime timestamp;
     private String cause;
+    private Long statusCode;
 
     public ExceptionResponse(String path, String cause) {
         this.path = path;
+        this.timestamp = LocalDateTime.now();
         this.cause = cause;
     }
 }
