a23354dd83d7e7e79d97e2a7fcb85258150d4b82
alllip <alllip@ttu.ee>
2023-11-15 15:09:10 +0200

    profile editing

--- src/main/java/ee/iti0302/veebiback/service/ProfileService.java
+++ src/main/java/ee/iti0302/veebiback/service/ProfileService.java
@@ -1,10 +1,12 @@
 package ee.iti0302.veebiback.service;
 
 import ee.iti0302.veebiback.domain.Person;
+import ee.iti0302.veebiback.domain.StatusCode;
 import ee.iti0302.veebiback.dto.BaseDto;
 import ee.iti0302.veebiback.dto.EditProfileDataDto;
 import ee.iti0302.veebiback.dto.ViewProfileDataDto;
 import ee.iti0302.veebiback.repository.PersonRepository;
+import ee.iti0302.veebiback.repository.StatusCodeRepository;
 import ee.iti0302.veebiback.service.mapper.PersonMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import java.util.Optional;
 public class ProfileService {
     private final PersonRepository personRepository;
     private final PersonMapper personMapper;
+    private final StatusCodeRepository statusCodeRepository;
 
     public ViewProfileDataDto getViewProfileData(Long id) {
         Optional<Person> optionalPerson = personRepository.findById(id);
@@ -40,6 +43,16 @@ public class ProfileService {
         if (optionalPerson.isPresent()) {
             Person person = optionalPerson.get();
             personMapper.updateProfileDataFromDto(dto, person);
+            /* StatusCode instance needs to be set manually, since mapping to
+             *  the already 'attached' StatusCode instance using update will result in an exception. */
+            if (dto.getRelationshipStatus() == null) {
+                person.setRelationshipStatus(null);
+            } else {
+                Optional<StatusCode> optionalStatusCode = statusCodeRepository.findByCode(
+                        dto.getRelationshipStatus().getCode()
+                );
+                person.setRelationshipStatus(optionalStatusCode.orElseThrow(RuntimeException::new));
+            }
             personRepository.save(person);
         }
         return new BaseDto();
--- src/main/java/ee/iti0302/veebiback/service/mapper/PersonMapper.java
+++ src/main/java/ee/iti0302/veebiback/service/mapper/PersonMapper.java
@@ -5,13 +5,20 @@ import ee.iti0302.veebiback.dto.EditProfileDataDto;
 import ee.iti0302.veebiback.dto.ViewProfileDataDto;
 import ee.iti0302.veebiback.dto.RegisterDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = {StatusCodeMapper.class})
 public interface PersonMapper {
     EditProfileDataDto toEditProfileDataDto(Person entity);
     Person registerDtoToEntity(RegisterDto dto);
+
+    /* If updating a Person entity that is still attached to the persistence context,
+    * you need to use the toEntity method to create a new StatusCode instance, since
+    * updating the attached StatusCode instance directly will result in an exception being thrown. */
+    @Mapping(target = "relationshipStatus", ignore = true)
     void updateProfileDataFromDto(EditProfileDataDto dto, @MappingTarget Person entity);
+    Person fromEditDtoToPerson(EditProfileDataDto dto);
     ViewProfileDataDto entityToProfileDataDto(Person entity);
 }
