621a21bb9f0fbc9a7551ae1d173e49445494322f
Markus <majoas@ttu.ee>
2023-12-02 23:43:31 +0200

    Feed page start

--- src/main/java/ee/iti0302/veebiback/service/PostService.java
+++ src/main/java/ee/iti0302/veebiback/service/PostService.java
@@ -1,18 +1,26 @@
 package ee.iti0302.veebiback.service;
 
+import ee.iti0302.veebiback.domain.Friendship;
 import ee.iti0302.veebiback.domain.Person;
 import ee.iti0302.veebiback.domain.Post;
-import ee.iti0302.veebiback.dto.AddPostDto;
-import ee.iti0302.veebiback.dto.BaseDto;
-import ee.iti0302.veebiback.dto.PostDto;
+import ee.iti0302.veebiback.dto.*;
+import ee.iti0302.veebiback.repository.FriendshipRepository;
 import ee.iti0302.veebiback.service.mapper.PostMapper;
 import ee.iti0302.veebiback.repository.PersonRepository;
 import ee.iti0302.veebiback.repository.PostRepository;
+import ee.iti0302.veebiback.service.mapper.StatusCodeMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static ee.iti0302.veebiback.util.enums.FriendshipStatus.FR_STATUS_A;
+import static ee.iti0302.veebiback.util.enums.FriendshipStatus.FR_STATUS_S;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +28,8 @@ public class PostService {
     private final PostRepository postRepository;
     private final PersonRepository personRepository;
     private final PostMapper postMapper;
+    private final FriendshipRepository friendshipRepository;
+    private final StatusCodeMapper statusCodeMapper;
 
     public List<PostDto> getPostList(Long personId) {
         List<Post> postList =
@@ -45,4 +55,21 @@ public class PostService {
         postRepository.deleteById(id);
         return new BaseDto();
     }
+
+    public List<PostDto> getFeed(Long personId, Integer page, Integer limit) {
+        List<Long> friends = getFriendIds(personId);
+        Pageable query = PageRequest.of(page, limit, Sort.by("createdAt").descending());
+        List<Post> posts = postRepository.findAllByPersonIdIn(friends, query);
+        return postMapper.toDtoList(posts);
+    }
+
+
+    private List<Long> getFriendIds(Long personId) {
+        String temp = "Accepted";
+        List<Friendship> friends = friendshipRepository.findFriendshipsByPersonIdAndStatusValue(personId, temp);
+        return friends.stream()
+                .map(fs -> fs.getFriend().getId())
+                .toList();
+    }
+
 }
